82e8ad3a1600c51458c34dae1cf2611d88e65c9e
Andrea Relova <andreamrelova@gmail.com>
2020-05-22 18:54:33 -0400

    created repo

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css"
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <div className="HeaderBar">
+        <h1>Portfolio</h1>
+        <h1>Andrea Relova</h1>
+        <h1>2020</h1>
+      </div>
+      <div className="Welcome2">
+        <h2>welcome!</h2>
+      </div>
+      <div className="Welcome">
+        <h2>welcome!</h2>
+      </div>
+      <div className="Tagline">
+        <p>My name is Andrea Relova. I'm a computer science <br></br>student at the University of Florida.</p>
+      </div>
+      <div className="Latest">
+        <h3>Latest Projects:</h3>
+      </div>
+      <footer className="FooterBar">
+        <h3>About Me</h3>
+        <h3>Contact</h3>
+        <h3>Resume</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
