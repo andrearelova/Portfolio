88514ca46e80ed98c85b579c430c8149ae603b42
Andrea Relova <andreamrelova@gmail.com>
2020-07-02 15:43:15 -0400

    Added resume link, started contact form.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css"
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import Slide from '@material-ui/core/Slide';
+import Grid from '@material-ui/core/Grid';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function App() {
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   return (
     <div className="App">
+      <Dialog open={dialogOpen} onClose={() => {setDialogOpen(false)}} TransitionComponent={Transition}>
+        <DialogContent>
+        <Grid>
+        <Grid item xs={12} md={6}>
+        <h1> Hello</h1>
+        </Grid>
+        <Grid item md={7}>
+        <h1> Hello</h1>
+        </Grid>
+        <Grid item>
+        <h1> Hello</h1>
+        </Grid>
+        </Grid>
+          <h1> Hello</h1>
+        </DialogContent>
+      </Dialog>
       <div className="HeaderBar">
         <h1>Portfolio</h1>
         <h1>Andrea Relova</h1>
@@ -24,9 +50,11 @@ function App() {
       </div>
       <footer className="FooterBar">
       <div className="FooterElement">
-        <h3>About Me</h3>
-        <h3>Contact</h3>
-        <h3>Resume</h3>
+        <h3 className="FooterButtons">About Me</h3>
+        <h3 className="FooterButtons" onClick={()=>{setDialogOpen(true)}}>Contact Me</h3>
+        <a href="AndreaRelova.pdf" target="blank" className="linkText">
+        <h3 className="FooterButtons" > Resume</h3>
+        </a>
         </div>
         <div className="FooterFiller"/>
       </footer>
