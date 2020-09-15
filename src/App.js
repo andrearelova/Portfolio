b34b7aed3b6a0ada149c0cc62d4bdc944c60b9c7
Andrea Relova <andreamrelova@gmail.com>
2020-09-14 22:55:52 -0400

    Added all projects to array, created about me dialog, fixed proj styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
 import React, {useState} from "react";
 import "./App.css"
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import Slide from '@material-ui/core/Slide';
-import Grid from '@material-ui/core/Grid';
+import {Dialog, DialogContent, Slide, Grid, TextField, Button} from "@material-ui/core"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 function App() {
-  const [dialogOpen, setDialogOpen] = useState(false);
+  const [contactOpen, setContactOpen] = useState(false);
+  const [aboutOpen, setAboutOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const projects = [
+    {
+      name:"Six:",
+      img:"Six.png",
+      description:"Six is a group web application created in July-August 2020 that is inspired by the \"Six Degrees of Kevin Bacon\" game. The web application asks the user to input two musical artists and then utilizes the Spotify Web API, along with a Breadth-First Search algorithm, to determine how many \"degrees of separation\" lie between the two artists (a song collaboration counts as one degree). I did the front end design and helped on the implementation of it. I also helped work on the API implementation. The project was created using React/Node.",
+      link:"https://github.com/andrearelova/Portfolio"
+    },
+    {
+      name:"DailyUI (Currently in Progress):",
+      img:"DailyUI2.png",
+      description:"DailyUI is an email subscription service that gives users a design prompt everyday, such as a landing page or an app icon. The picture above is from Day 2's prompt, which was to design a checkout page. My designs were made with Adobe XD.",
+      link:""
+    },
+    {
+      name:"Portfolio (This Website):",
+      img:"portfolio-mockup.png",
+      description:"This portfolio website was created throughout the summer of 2020. The mockup above was made on Photoshop (because I created it before knowing how to use Adobe XD). The portfolio was made with React/Node and uses some Material UI styling components.",
+      link:""
+    },
+    {
+      name:"Some Other Designs:",
+      img:"",
+      description:"These are some other designs I have made, either for myself, friends, or some school organizations I am involved in. The details, as well as the software used to make each one, is specified above.",
+      link:""
+    },
+  ]
+  const handleContactClose = () => {
+    setContactOpen(false);
+  }
+  const submitEmail = (e) => {
+    e.preventDefault();
 
+
+
+    handleContactClose();
+  }
   return (
     <div className="App">
-      <Dialog open={dialogOpen} onClose={() => {setDialogOpen(false)}} TransitionComponent={Transition}>
-        <DialogContent>
-        <Grid>
-        <Grid item xs={12} md={6}>
-        <h1> Hello</h1>
-        </Grid>
-        <Grid item md={7}>
-        <h1> Hello</h1>
-        </Grid>
-        <Grid item>
-        <h1> Hello</h1>
-        </Grid>
-        </Grid>
-          <h1> Hello</h1>
+      <Dialog open={contactOpen} onClose={handleContactClose} TransitionComponent={Transition}>
+        <DialogContent className="ContactMe">
+        <h1>Send a message to andreamrelova@gmail.com:</h1>
+        <form  className="ContactForm" onSubmit={submitEmail} >
+          <TextField id="outlined-basic" label="Your Email" required={true} value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+          <TextField id="outlined-basic" label="Message" required={true} value={message} onChange={(e)=>{setMessage(e.target.value)}}/>
+          <Button type="submit">Send</Button>
+        </form>
         </DialogContent>
       </Dialog>
       <div className="HeaderBar">
@@ -47,27 +71,39 @@ function App() {
       </div>
         <p className="Tagline">My name is Andrea Relova. I'm a computer science student at the University of Florida.<br></br>I strongly believe that design can be both beautiful and accessible and I<br></br>plan on pursuing a career that helps me find the intersection of both.</p>
         <h3 className="Latest">Latest Projects:</h3>
-        <h3 className="Projects">Six:</h3>
-        <Card className="ProjectsCard" variant="outlined">
-          <CardActionArea>
-            <CardMedia image="./Six.png" title="Six Screenshot"/>
-            <CardContent className="ProjectsContent">
-              <p>hi</p>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <h3 className="Projects">DailyUI:</h3>
-        <h3 className="Projects">Portfolio (This Website):</h3>
-        <h3 className="Projects">Some Other Designs:</h3>
-        <h3 className="Projects">More Projects Coming soon!</h3>
+        {projects.map((project, index)=>
+          <a className="Link" key={index} href={project.link} target="blank">
+          <div className="Project">
+            <h3 className="Projects">{project.name}</h3>
+              <img src={project.img} className="ProjectsMedia"/>
+            <p className="ProjectsDescription">{project.description}</p>
+          </div>
+          </a>
+        )}
+        <h3 className="Latest">More Projects Coming Soon!</h3>
       </div>
       <footer className="FooterBar">
       <div className="FooterElement">
-        <h3 className="FooterButtons">About Me</h3>
-        <h3 className="FooterButtons" onClick={()=>{setDialogOpen(true)}}>Contact Me</h3>
+        <h3 className="FooterButtons"onClick={()=>{setAboutOpen(true)}}>About Me</h3>
+        <h3 className="FooterButtons" onClick={()=>{setContactOpen(true)}}>Contact Me</h3>
         <a href="AndreaRelova.pdf" target="blank" className="linkText">
-        <h3 className="FooterButtons"> Resume</h3>
+          <h3 className="FooterButtons"> Resume</h3>
         </a>
+        <a href="https://github.com/andrearelova" target="blank" className="linkText">
+          <h3 className="FooterButtons"> GitHub</h3>
+        </a>
+        <a href="https://www.linkedin.com/in/andrea-relova-206163194/" target="blank" className="linkText">
+          <h3 className="FooterButtons"> LinkedIn</h3>
+        </a>
+        <a href="https://dribbble.com/andrearelova" target="blank" className="linkText">
+          <h3 className="FooterButtons"> Dribbble</h3>
+        </a>
+        <Dialog open={aboutOpen} onClose={() => {setAboutOpen(false)}} TransitionComponent={Transition}>
+          <DialogContent className="AboutMe">
+            <h1 className="AboutMeTitle">About Me</h1>
+            <p className="AboutMeText">I'm a 20 year old computer science major from Jacksonville, Florida. I am also pursuing a minor in linguistics, which I chose because my experience with learning French and Spanish in school led me to realize that my passion for languages lies with learning about the structure, patterns and uses of it and less so with a specific language. I am very interested in interaction design, product design and user experience design and would like to pursue a career in such someday. I realized this because in my computer science projects, I have found the greatest satisfaction and sense of control from the designing portions of them. I continue to work on developing my "eye for design" and my sense of empathy everyday, since being a designer involves one putting themselves in the users' shoes.</p>
+          </DialogContent>
+        </Dialog>
         </div>
         <div className="FooterFiller"/>
       </footer>
